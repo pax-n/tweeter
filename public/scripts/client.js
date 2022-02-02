15a1179fbe32a5409851b48fafafd3a1e59f6ba3
pax-n <paxton_nguyen@hotmail.com>
2022-02-02 16:56:32 -0500

    dynamic tweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,3 +3,64 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  }
+
+  const createTweetElement = function(tweet) {
+    let $tweet = `
+    <article class="tweet">
+      <header>
+        <span class="profile">
+          <img src="${tweet.user.avatars}">
+          <span>${tweet.user.name}</span> 
+        </span>
+        <span class="user">${tweet.user.handle}</span>
+      </header>
+      <span class="words">${tweet.content.text}</span>
+      <footer>
+        <span>${timeago.format(tweet.created_at)}</span>
+        <span class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart">'</i>
+        </span>
+      </footer>
+    </article>
+    `;
+    return $tweet;
+  }
+
+  renderTweets(tweetData);
+});

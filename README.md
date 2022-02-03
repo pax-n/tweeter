692e404a50110be11e15e944a0be3c83154002f6
pax-n <paxton_nguyen@hotmail.com>
2022-02-03 18:18:07 -0500

    final commit

--- README.md
+++ README.md
@@ -2,17 +2,31 @@
 
 Tweeter is a simple, single-page Twitter clone.
 
-This repository is the starter code for the project: Students will fork and clone this repository, then build upon it to practice their HTML, CSS, JS, jQuery and AJAX front-end skills, and their Node, Express back-end skills.
+Tweeting to nobody could be as fun as tweeting to people you don't even know!
+
+## Final Product
+
+!["Screenshot of desktop version"]()
+
+!["Screenshot of tablet version"]()
 
 ## Getting Started
 
 1. [Create](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) a new repository using this repository as a template.
 2. Clone your repository onto your local device.
 3. Install dependencies using the `npm install` command.
-3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
+3. Start the web server using the `npm start` command. The app will be served at <http://localhost:8080/>.
 4. Go to <http://localhost:8080/> in your browser.
 
 ## Dependencies
 
 - Express
 - Node 5.10.x or above
+
+## Using Tweeter
+
+#### Tweet
+
+There's only one function to this app which is tweet due being only a front end design focused project.
+
+Writing something inside the text box and pressing tweet will generate random tweets from random users.
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,37 +6,39 @@
 $(document).ready(function() {
 
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const tweetData = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
+  //
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
+  //html layout for new tweets from ajax
   const createTweetElement = function(tweet) {
     let $tweet = `
     <article class="tweet">
@@ -59,38 +61,45 @@ $(document).ready(function() {
     </article>
     `;
     return $tweet;
-  }
+  };
 
+  //renders the tweet onto the page
   const renderTweets = function(tweets) {
     $('#tweets-container').empty();
     for (let tweet of tweets) {
       const $tweet = createTweetElement(tweet);
       $('#tweets-container').prepend($tweet);
     }
-  }
+  };
 
+  //asynchronous http request (get) to retrieve data from fake server (/tweets).
   const loadTweets = function () {
     $.ajax('/tweets', {method: 'GET'})
     .then(function (tweets) {
-      console.log('Success: ', tweets);
-      renderTweets(tweets)
-    })
-  }
+      // console.log('Success: ', tweets);
+      renderTweets(tweets);
+    });
+  };
 
-  $("#warning").hide()
+  //hides warning header from loading up on main page
+  $("#warning").hide();
 
+  //submit function for button
   $( "#submit" ).submit(function( event ) {
-    console.log("form submitted");
     event.preventDefault();
-    const param = $(this).serialize();
-    console.log(param);
+    // const param = $(this).serialize();
+    // console.log(param);
 
     let text = $("form").find("textarea").val();
-    if(text.length > 140) {
+
+    //if characters in text input exceed 140 characters, display message
+    if (text.length > 140) {
       return $("#warning").text("Characters exceeding the max LIMIT!").slideDown(400);
+      //if no characters or only spaces are entered, display message
     } else if (text.length === null || text.length === 0 || text.trim() === "") {
       return $("#warning").text("Tweet cannot be EMPTY!").slideDown(400);
     } else {
+      //will display new tweet without refreshing home page
       $.ajax({
         url: "/tweets",
         data: $(this).serialize(), 
@@ -99,10 +108,11 @@ $(document).ready(function() {
           $("form").find("textarea").val('');
           loadTweets();
         }
-      })
-      $("#warning").hide()
-    } 
+      });
+      //hides warning header if the header was displayed previously
+      $("#warning").hide();
+    }
   });
 
-  loadTweets()
+  loadTweets();
 });
